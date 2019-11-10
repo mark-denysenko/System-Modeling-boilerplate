e5394a9cf4da9f8b409d3b26c5179aa94aba0653
Mark <mark.denyssenko@gmail.com>
2019-11-10 21:58:01 +0200

    Implemented schemas

--- ModelingClient/Program.cs
+++ ModelingClient/Program.cs
@@ -3,21 +3,104 @@ using Domain.Modeling.BaseEntities;
 using Domain.RandomNumberGenerators;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ModelingClient
 {
     class Program
     {
+        private static double SimulateTime = 10_000d;
+
         static void Main(string[] args)
         {
-            Lab3();
+            //Lab3();
+            Console.WriteLine("Simulate dynamic systems");
+
+            int N = 0;
+            bool isInputCorrect = true;
+
+            while (isInputCorrect || N > 0)
+            {
+                Console.Write("Simulate for N = ");
+                isInputCorrect = int.TryParse(Console.ReadLine(), out N);
+
+                if (!isInputCorrect)
+                    break;
+
+                LinearSchemaGenerator(N);
+                SchemaWithQuickWaysGenerator(N);
+            }
+
             Console.ReadKey();
+        }
+
+        private static void LinearSchemaGenerator(int N)
+        {
+            const double creatorDelay = 1.0d;
+            var creatorGenerator = RandomGeneratorsFactory.GetExponential(creatorDelay);
+            const double processDelay = 1.0d;
+            var processorGenerator = RandomGeneratorsFactory.GetExponential(processDelay);
+
+            var creator = new Creator<EventBase>("creator", creatorGenerator);
+            var despose = new Disposer("despose");
+            var elementsList = new List<SchemaElementBase>(N + 2)
+            {
+                creator,
+                despose
+            };
+
+            SchemaElementBase previosElement = creator;
+            foreach(var index in Enumerable.Range(0, N))
+            {
+                var newProcessor = new Processor("processor" + index, processorGenerator, 100, 1);
+                previosElement.NextElement = newProcessor;
+                elementsList.Add(newProcessor);
+
+                previosElement = newProcessor;
+            }
+            previosElement.NextElement = despose;
+
+            Console.WriteLine("Simualtion linear model " + DateTime.Now);
+            var simulationModel = new EventBasedSimulationModel(elementsList, SimulateTime);
+            simulationModel.Simulate(false);
+            simulationModel.PrintResultStatistic();
+        }
+
+        private static void SchemaWithQuickWaysGenerator(int N)
+        {
+            const double creatorDelay = 1.0f;
+            var creatorGenerator = RandomGeneratorsFactory.GetExponential(creatorDelay);
+            const double processDelay = 1.0f;
+            var processorGenerator = RandomGeneratorsFactory.GetExponential(processDelay);
+
+            var creator = new Creator<EventBase>("creator", creatorGenerator);
+            var despose = new Disposer("despose");
+            var elementsList = new List<SchemaElementBase>(N + 2)
+            {
+                creator,
+                despose
+            };
+
+            SchemaElementBase previosElement = creator;
+            foreach (var index in Enumerable.Range(0, N))
+            {
+                var newProcessor = new Processor("processor" + index, processorGenerator, 100, 1);
+                elementsList.Add(newProcessor);
+                var newBranch = new Branch(new List<(SchemaElementBase element, double percent)> { (newProcessor, 0.95), (despose, 0.05) });
+                previosElement.NextElement = newBranch;
+
+                previosElement = newProcessor;
+            }
+            previosElement.NextElement = despose;
+
+            Console.WriteLine("Simualtion linear model with quick way " + DateTime.Now);
+            var simulationModel = new EventBasedSimulationModel(elementsList, SimulateTime);
+            simulationModel.Simulate(false);
+            simulationModel.PrintResultStatistic();
         }
 
         private static void Lab3()
         {
-            double SimulateTime = 10_000d;
-
             const double creatorDelay = 1.0f;
             var creatorGenerator = RandomGeneratorsFactory.GetExponential(creatorDelay);
             const double processDelay = 2.5f;
